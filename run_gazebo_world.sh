ba3324236ffddf5e7708d6f6d0468cfa039d6694
Alberto S. Naranjo Galet <155007+Veilkrand@users.noreply.github.com>
2018-12-05 21:05:37 +0900

    new common script for sourcing

--- run_gazebo_world.sh
+++ run_gazebo_world.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # Source Gazebo model path to include this package models
-export GAZEBO_MODEL_PATH=$(pwd)/models:$GAZEBO_MODEL_PATH
-# export GAZEBO_RESOURCE_PATH=GAZEBO_RESOURCE_PATH:$(pwd)/worlds
+. $(pwd)/gazebo_assets_setup.sh
 
 roslaunch gazebo_assets_drone_race test_track.launch
