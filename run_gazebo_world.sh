c58caf665ace1fdd617b7f45fa9a993dd638300a
Alberto S. Naranjo Galet <155007+Veilkrand@users.noreply.github.com>
2018-12-05 20:46:25 +0900

    Fixed source path for models

--- run_gazebo_world.sh
+++ run_gazebo_world.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Source Gazebo model path to include this package models
-export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(pwd)/models
-# export GAZEBO_RESOURCE_PATH=path/to/worlds.
+export GAZEBO_MODEL_PATH=$(pwd)/models:$GAZEBO_MODEL_PATH
+# export GAZEBO_RESOURCE_PATH=GAZEBO_RESOURCE_PATH:$(pwd)/worlds
 
 roslaunch gazebo_assets_drone_race test_track.launch
