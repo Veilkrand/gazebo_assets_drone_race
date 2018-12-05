010f8e0935e0876c7b9545fbf53743e04ad598b2
Alberto <155007+Veilkrand@users.noreply.github.com>
2018-12-05 16:21:17 +0900

    Update run_gazebo_world.sh

--- run_gazebo_world.sh
+++ run_gazebo_world.sh
@@ -2,5 +2,6 @@
 
 # Source Gazebo model path to include this package models
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$(pwd)/models
+# export GAZEBO_RESOURCE_PATH=path/to/worlds.
 
 roslaunch gazebo_assets_drone_race test_track.launch
