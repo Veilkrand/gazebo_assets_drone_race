ad18a52813d2cdf600d1b7a7afccea4a0980df10
Alberto S. Naranjo Galet <155007+Veilkrand@users.noreply.github.com>
2018-12-07 11:30:00 +0900

    source script using rospackage find

--- gazebo_assets_setup.sh
+++ gazebo_assets_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Source Gazebo model path to include this package models
-export GAZEBO_MODEL_PATH=$(pwd)/models:$GAZEBO_MODEL_PATH
+export GAZEBO_MODEL_PATH=$(rospack find gazebo_assets_drone_race)/models:$GAZEBO_MODEL_PATH
 # export GAZEBO_RESOURCE_PATH=GAZEBO_RESOURCE_PATH:$(pwd)/worlds
