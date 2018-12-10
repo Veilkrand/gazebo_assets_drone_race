8af2d10e4a5b3a76fee9d0dcb7cf03f0d0acd8d6
Alberto S. Naranjo Galet <155007+Veilkrand@users.noreply.github.com>
2018-12-10 21:00:30 +0900

    Improved script and cleanup

--- tools/gazebo_world_parser/test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python world_objects_to_file.py test_track.world aruco_4x4 '4x4_(\d\d?)'
--- tools/gazebo_world_parser/world_objects_to_file.py
+++ tools/gazebo_world_parser/world_objects_to_file.py
@@ -6,65 +6,67 @@ import argparse
 
 def parse_pose(pose):
 
-	p_list = pose.split(' ')
+    p_list = pose.split(' ')
 
-	return (float(p_list[0]), float(p_list[1]), float(p_list[2]), float(p_list[3]), float(p_list[4]), float(p_list[5]))
+    return (float(p_list[0]), float(p_list[1]), float(p_list[2]), float(p_list[3]), float(p_list[4]), float(p_list[5]))
 
 def get_id_text_from_name(name, regex):
-	''' It needs to be changed per case'''
+    ''' It needs to be changed per case'''
 
-	ids = re.compile(regex)
-	match = ids.search(name)
-	if match:
-		return match.group(1)
-	else:
-		return '0'
+    ids = re.compile(regex)
+    match = ids.search(name)
+    if match:
+	    return match.group(1)
+    else:
+	    return '0'
 
 
 if __name__=='__main__' :
 
 	
-	parser = argparse.ArgumentParser()
-	parser.add_argument("input_file", help="Path to the .world file to process")
-	parser.add_argument("object_name", help="String that the name of the object starts with, e.g.: 'aruco_4x4'")
-	parser.add_argument("regex", help="Regex rule string to match and extract the ID from the name of the object, e.g.: '4x4_(\d\d?)'")
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help="Path to the .world file to process")
+    parser.add_argument("object_name", help="String that the name of the object starts with, e.g.: 'aruco_4x4'")
+    parser.add_argument("regex", help="Regex rule string to match and extract the ID from the name of the object, e.g.: '4x4_(\d\d?)'")
+    args = parser.parse_args()
 
-	#_filename = 'test_track.world'
-	_filename = args.input_file
-	_output_filename = 'output.json'
-	#_regex = r"4x4_(\d\d?)"
-	_regex = args.regex
-	#_object_name = 'aruco_4x4'
-	_object_name = args.object_name
+    #_filename = 'test_track.world'
+    _filename = args.input_file
+    _output_filename = 'output.json'
+    #_regex = r"4x4_(\d\d?)"
+    _regex = args.regex
+    #_object_name = 'aruco_4x4'
+    _object_name = args.object_name
 
 
-	xmldoc = minidom.parse(_filename)
-	models = xmldoc.getElementsByTagName('model')
+    xmldoc = minidom.parse(_filename)
+    world = xmldoc.getElementsByTagName('world')
 
-	output = list()
+    output = list()
 
-	for model in models:
-		name = model.attributes['name'].value 
-		if name.startswith( _object_name):
-			model_object = {}
-			model_object['name'] = name
-			#print name
-			model_object['id'] = get_id_text_from_name(name,_regex)
-			for t in model.childNodes:
-				if t.nodeName == 'pose':
-					pose_text = t.firstChild.nodeValue
-					model_object['pose'] = parse_pose(pose_text)
-					break
-			print name, model_object['id'], model_object['pose']
-			output.append(model_object)
-
-	if len(output) > 0:
-		#print json.dumps(output, sort_keys = True, indent = 4, ensure_ascii = False)
-		with io.open(_output_filename, 'w', encoding="utf-8") as outfile:
-			outfile.write(unicode(json.dumps(output, sort_keys = True, indent = 4, ensure_ascii = False)))
-			outfile.close()
-			print '* Saved output to', _output_filename
+    for model in world[0].childNodes:
+        # print model.nodeName
+        if model.nodeName == 'model':
+            model_name = model.attributes['name'].value
+            if model_name.startswith( _object_name):
+                model_object = {}
+                model_object['name'] = model_name
+                #print model_name
+                model_object['id'] = get_id_text_from_name(model_name,_regex)
+                for t in model.childNodes:
+                    if t.nodeName == 'pose':
+                        pose_text = t.firstChild.nodeValue
+                        model_object['pose'] = parse_pose(pose_text)
+                        break
+                print model_name, model_object['id'], model_object['pose']
+                output.append(model_object)
+                        
+    if len(output) > 0:
+	    #print json.dumps(output, sort_keys = True, indent = 4, ensure_ascii = False)
+	    with io.open(_output_filename, 'w', encoding="utf-8") as outfile:
+		    outfile.write(unicode(json.dumps(output, sort_keys = True, indent = 4, ensure_ascii = False)))
+		    outfile.close()
+		    print '* Saved output to', _output_filename
 
 
 
